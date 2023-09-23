a56b8dc2ba673408aee0bc8212ea34badcfec53b
Pudge596 <135785375+Pudge596@users.noreply.github.com>
2023-09-23 14:11:53 +0700

    d
    d

---
+++ WpfApp1/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(numberInput.Text);
+                string chislo;
+
+                if (number == 0)
+                {
+                    chislo = "нулевое число";
+                }
+                else
+                {
+                    string sign = (number < 0) ? "отрицательное" : "положительное";
+                    int absNumber = Math.Abs(number);
+
+                    if (absNumber < 10)
+                    {
+                        chislo = $"{sign} однозначное число";
+                    }
+                    else if (absNumber < 100)
+                    {
+                        chislo = $"{sign} двузначное число";
+                    }
+                    else
+                    {
+                        chislo = $"{sign} трехзначное число";
+                    }
+                }
+
+                descriptionOutput.Text = $"Описание числа: {chislo}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректное целое число.");
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Window2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string inputText = inputTextBox.Text;
+
+            // Разбиваем строку на слова, используя пробелы в качестве разделителей
+            string[] words = inputText.Split(new char[] { ' ', ',','.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int maxLength = words.Max(word => word.Length);
+
+            resultTextBlock.Text = $"Самое длинное слово имеет длину {maxLength} символов.";
+        }
+    }
+}
+   
